608fe5e2f0c6ec447e4a6992ad5ccda84a5e7560
‘jiaoyou’ <18898159853@163.com>
2024-01-10 10:42:22 +0800

    上传图片优化

--- router/user.js
+++ router/user.js
@@ -35,6 +35,9 @@ const storage = multer.diskStorage({
   },
   // 确定文件名
   filename(req, file, cb) {
+    file.originalname=Buffer.from(file.originalname, "latin1").toString(
+      "utf8"
+  );
     cb(null,  file.originalname)
   }
 })
